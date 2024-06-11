86531a202a2729f2b1e283cc740340815bdbd634
astrowang <819369336@qq.com>
2024-06-11 22:28:44 +0800

    add jump

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,18 +1,62 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.TextCore.Text;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public PlayerInputControls inputControl;
+    private Rigidbody2D rb;
+    private float speed = 260;
+    private float jumpForce = 10.0f;
+    public Vector2 inputDirection;
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        inputControl = new PlayerInputControls();
+        //跳跃
+        inputControl.Gameplay.Jump.started += Jump;
     }
 
-    // Update is called once per frame
+    private void OnEnable()
+    {
+        inputControl.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputControl.Disable();
+    }
+
     void Update()
     {
-        
+        inputDirection = inputControl.Gameplay.Move.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        //人物移动
+        rb.velocity = new Vector2(inputDirection.x * speed * Time.deltaTime, rb.velocity.y);
+        int faceDir = (int)transform.localScale.x;
+        if (inputDirection.x > 0)
+            faceDir = 1;
+        if (inputDirection.x < 0)
+            faceDir = -1;
+
+        //人物翻转
+        transform.localScale = new Vector3(faceDir, 1, 1);
+    }
+
+    private void Jump(InputAction.CallbackContext obj)
+    {
+        Debug.Log("JUMP");
+        rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
     }
 }
