856232a472dc1ea4819968ab25aeeb523c86cfb7
819369336@qq.com <819369336@qq.com>
2024-07-02 20:53:11 +0800

    add boar turn on wall

--- Assets/Scripts/General/PhysicsCheck.cs
+++ Assets/Scripts/General/PhysicsCheck.cs
@@ -4,10 +4,28 @@ using UnityEngine;
 
 public class PhysicsCheck : MonoBehaviour
 {
-    public bool isGround;
+    private CapsuleCollider2D coll;
+    [Header("¼ì²â²ÎÊý")]
+    public bool manual;
     public float checkRaduis;
     public Vector2 bottomOffset;
+    public Vector2 leftOffset;
+    public Vector2 rightOffset;
     public LayerMask groundLayer;
+    [Header("×´Ì¬")]
+    public bool isGround;
+    public bool touchLeftWall;
+    public bool touchRightWall;
+    private void Awake()
+    {
+        coll = GetComponent<CapsuleCollider2D>();
+
+        if (!manual)
+        {
+            rightOffset = new Vector2((coll.bounds.size.x + coll.offset.x) / 2, coll.bounds.size.y / 2);
+            leftOffset = new Vector2(-rightOffset.x, rightOffset.y);
+        }
+    }
 
     void Update()
     {
@@ -16,11 +34,18 @@ public class PhysicsCheck : MonoBehaviour
 
     public void Check()
     {
+        //¼ì²âµØÃæ
         isGround = Physics2D.OverlapCircle((Vector2)transform.position + new Vector2(bottomOffset.x * transform.localScale.x, bottomOffset.y), checkRaduis, groundLayer);
+
+        //Ç½ÌåÅÐ¶Ï
+        touchLeftWall = Physics2D.OverlapCircle((Vector2)transform.position + new Vector2(leftOffset.x, leftOffset.y), checkRaduis, groundLayer);
+        touchRightWall = Physics2D.OverlapCircle((Vector2)transform.position + new Vector2(rightOffset.x, rightOffset.y), checkRaduis, groundLayer);
     }
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.DrawWireSphere((Vector2)transform.position + new Vector2(bottomOffset.x * transform.localScale.x, bottomOffset.y), checkRaduis);
+        Gizmos.DrawWireSphere((Vector2)transform.position + new Vector2(leftOffset.x, leftOffset.y), checkRaduis);
+        Gizmos.DrawWireSphere((Vector2)transform.position + new Vector2(rightOffset.x, rightOffset.y), checkRaduis);
     }
 }
