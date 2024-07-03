a0734305143c4e98005a964b00b0c7628d086fb3
819369336@qq.com <819369336@qq.com>
2024-07-03 21:26:18 +0800

    add boar hit and dead

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
@@ -13,11 +14,17 @@ public class Enemy : MonoBehaviour
     public float chaseSpeed;
     [HideInInspector] public float currentSpeed;
     public Vector3 faceDir;
+    public Transform attacker;
+    public float hurtForce;
 
     [Header("计时器")]
     public float waitTime;
     public float waitTimeCounter;
     public bool wait;
+
+    [Header("状态")]
+    public bool isHurt;
+    public bool isDead;
 
     protected virtual void Awake()
     {
@@ -41,7 +48,7 @@ public class Enemy : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (!wait)
+        if (!wait && !isHurt)
             Move();
     }
 
@@ -50,6 +57,9 @@ public class Enemy : MonoBehaviour
         rb.velocity = new Vector2(currentSpeed * faceDir.x * Time.deltaTime, rb.velocity.y);
     }
 
+    /// <summary>
+    /// 计时器
+    /// </summary>
     public void TimeCounter()
     {
         if (wait) // 撞墙停2s
@@ -62,5 +72,41 @@ public class Enemy : MonoBehaviour
                 transform.localScale = new Vector3(faceDir.x, 1, 1);
             }
         }
+    }
+
+    public void OnTakeDamage(Transform attackTrans)
+    {
+        attacker = attackTrans;
+        //转身
+        if (attackTrans.position.x - transform.position.x > 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+        if (attackTrans.position.x - transform.position.x < 0)
+            transform.localScale = new Vector3(1, 1, 1);
+
+        //受伤被击退
+        isHurt = true;
+        anim.SetTrigger("hurt");
+        Vector2 dir = new Vector2(transform.position.x - attackTrans.position.x, 0).normalized;
+        //rb.velocity = new Vector2(0, rb.velocity.y);
+        StartCoroutine(OnHurt(dir));
+    }
+
+    private IEnumerator OnHurt(Vector2 dir)
+    {
+        rb.AddForce(dir * hurtForce, ForceMode2D.Impulse);
+        yield return new WaitForSeconds(0.5f);
+        isHurt = false;
+    }
+
+    public void OnDie()
+    {
+        gameObject.layer = 2; // 避免Boar的capsule继续伤害Player，还需要在设置里改这层layer
+        anim.SetBool("dead", true);
+        isDead = true;
+    }
+
+    public void DestroyAfterAnimation()
+    {
+        Destroy(this.gameObject);
     }
 }
